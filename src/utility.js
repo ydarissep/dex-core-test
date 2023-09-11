8a697d11ed5e5383ddd84274115d99469738510d
ydarissep <60859033+ydarissep@users.noreply.github.com>
2023-09-11 02:21:53 +0200

    Add files via upload

--- src/utility.js
+++ src/utility.js
@@ -5,3 +5,17 @@ fetch("https://raw.githubusercontent.com/ydarissep/dex-core/main/src/utility.js"
 }).catch(error => {
     console.warn(error)
 })
+
+
+
+
+
+
+async function forceUpdate(){
+    const update = 18
+    if(localStorage.getItem("update") != `${update} IR`){
+        await localStorage.clear()
+        await localStorage.setItem("update", `${update} IR`)
+        await footerP("Fetching data please wait... this is only run once")
+    }
+}
